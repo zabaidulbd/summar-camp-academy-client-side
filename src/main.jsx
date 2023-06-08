a97d3764133510699bf4db8966612f03e0cdd028
zabaidulbd <zislam5622@gmail.com>
2023-06-08 14:13:01 +0600

    add manage user

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,29 @@ import {
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
 import AuthProvider from './Providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
+
+
+
+
+
+
+
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        < RouterProvider router={router} />
+      </QueryClientProvider>
     </AuthProvider>
 
   </React.StrictMode>,
--- src/pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,9 +1,59 @@
+import { useQuery } from "@tanstack/react-query";
+// import { useState } from "react";
 
 
 const ManageUsers = () => {
+
+    // const [disabled, setDisabled] = useState(false)
+
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await fetch('http://localhost:5000/users')
+        return res.json();
+    })
+
+
     return (
         <div>
-            <h>from manage user</h>
+            <h1>Total user : {users.length} </h1>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    {
+                                        user.role === 'admin' ? 'admin' : <button className="btn btn-active btn-ghost btn-sm">Make Admin</button>
+                                    }
+
+                                </td>
+                                <td>
+                                    {
+                                        user.role === 'instructor' ? 'instructor' : <button className="btn btn-active btn-ghost btn-sm">Make Instructor</button>
+                                    }
+
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+
+
         </div>
     );
 };
