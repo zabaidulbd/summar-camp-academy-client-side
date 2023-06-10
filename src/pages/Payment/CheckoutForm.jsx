e27ea0033cc209b2600e0f2dd78cac3d7339ae3e
zabaidulbd <zislam5622@gmail.com>
2023-06-10 12:50:23 +0600

    add portion of payment

---
+++ src/pages/Payment/CheckoutForm.jsx
@@ -0,0 +1,72 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+
+
+const CheckoutForm = () => {
+
+
+    const stripe = useStripe();
+    const elements = useElements();
+    const [cardError, setCardError] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!stripe || !elements) {
+            return
+        }
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if (error) {
+            console.log('error', error)
+            setCardError(error.message);
+
+        }
+        else {
+            setCardError('');
+            console.log('payment method', paymentMethod)
+        }
+
+
+    }
+
+
+
+
+
+    return (
+        <>
+            <form className="w-2/3 m-8" onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button className="btn btn-warning btn-sm mt-4" type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            {cardError && <h6 className="text-amber-500 ml-9">{cardError}</h6>}
+        </>
+
+    );
+};
+
+export default CheckoutForm;
--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,9 +1,20 @@
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
 
 const Payment = () => {
+
+    const stripePromise = loadStripe(import.meta.env.VITE_PAY_PK);
+
+
+
+
     return (
-        <div>
-            <h1>from payment page</h1>
+        <div className="w-3/4">
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
         </div>
     );
 };
