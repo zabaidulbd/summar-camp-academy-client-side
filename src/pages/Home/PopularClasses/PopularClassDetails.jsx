f959e6817384f8aa203cc8af90159272427b7b50
zabaidulbd <zislam5622@gmail.com>
2023-06-12 14:17:09 +0600

    remove codes from popular class

--- src/pages/Home/PopularClasses/PopularClassDetails.jsx
+++ src/pages/Home/PopularClasses/PopularClassDetails.jsx
@@ -24,27 +24,6 @@ const PopularClassDetails = ({ singleClass }) => {
             </div>
             <figure><img className="h-80 w-full" src={image} alt="Shoes" /></figure>
         </div>
-
-
-
-
-        // <div className="card w-96 bg-base-100 shadow-xl image-full">
-        //     <figure><img src={image} alt="class" /></figure>
-        //     <div className="card-body mt-5">
-        //         <h2 className="card-title ms-5 text-white"> Class Name : {name}
-        //             <div className="badge badge-secondary">popular</div>
-        //         </h2>
-        //         <div>
-        //             <p className="text-center text-white font-semibold mt-3">Instructor : {instructorName}</p>
-        //         </div>
-        //         <div className="flex justify-between ms-10">
-        //             <p className="text-white font-semibold"> Price : {price}</p>
-        //             <p className="text-white font-semibold"> Available Seats : {seat}</p>
-        //         </div>
-        //         <button className="btn btn-neutral mt-48">View Details</button>
-        //     </div>
-
-        // </div>
     );
 };
 
