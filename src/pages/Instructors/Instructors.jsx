518601fa6ba3dc2cb889dbab89a00c59c7c8b14c
zabaidulbd <zislam5622@gmail.com>
2023-08-25 11:56:10 +0600

    fournish intructor page

--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -1,58 +1,25 @@
 import useUser from "../../hooks/useUser";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from "react";
+import InstructorsDetails from "./InstructorsDetails";
 
 
 const Instructors = () => {
     const [user] = useUser();
     const instructor = user.filter(singleUser => singleUser.role === 'instructor');
 
-    useEffect(() => {
-        AOS.init({
-            duration: 1000,
-            once: true,
-        });
-        AOS.refresh(); // Refresh AOS when the component mounts or updates
-    }, []);
+
 
     return (
-        <div className="bg-red-200">
-            <h1 data-aos="fade-up" className="font-bold text-5xl text-center pb-5 pt-20">List of Instructors in our Academy</h1>
-            <div data-aos="fade-down" className="overflow-x-auto p-14">
-                <table className="table">
-                    {/* head */}
-                    <thead className="bg-red-400 text-white fw-semibold hover:bg-red-600">
-                        <tr className="text-lg">
-                            <th></th>
-                            <th>Image</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody className="bg-red-200 text-gray-950 text-base">
-                        {
-                            instructor.map(singleInstructor => <tr key={singleInstructor._id}>
-                                <td></td>
-                                <td>
-                                    <div className="flex items-center space-x-3">
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle w-14 h-12">
-                                                <img src={singleInstructor.image} alt="Instructor's image" />
-                                            </div>
-                                        </div>
+        <div className="mt-20 mb-28">
+            <div className="text-center mb-28 mx-20">
+                <h1 className="font-bold text-5xl text-gray-900 mb-8 underline">Meet Our Instructors</h1>
+                <h2 className="font-bold text-3xl text-gray-800">Discover the knowledgeable and dedicated instructors who make our academy exceptional and learn about their expertise and teaching philosophy.</h2>
+            </div>
 
-                                    </div>
-                                </td>
-                                <td>{singleInstructor.name}</td>
-                                <td>{singleInstructor.email}</td>
-                                <td><button className="btn btn-active btn-error text-white hover:bg-red-600">Details</button></td>
-                            </tr>)
-                        }
-                    </tbody>
 
-                </table>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-28 p-10">
+                {
+                    instructor.map(singleInstructor => <InstructorsDetails key={singleInstructor._id} singleInstructor={singleInstructor} ></InstructorsDetails>)
+                }
             </div>
 
         </div>
---
+++ src/pages/Instructors/InstructorsDetails.jsx
@@ -0,0 +1,84 @@
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { useEffect, useState } from 'react';
+import {
+    FaFacebook,
+    FaTwitter,
+    FaInstagram,
+    FaLinkedin,
+    FaGithub,
+} from 'react-icons/fa';
+
+const InstructorsDetails = ({ singleInstructor }) => {
+    const { name, image, email } = singleInstructor;
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: true,
+        });
+        AOS.refresh();
+    }, []);
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
+        <div
+            className={`bg-gray-100 ${isHovered ? '' : 'skew-y-12 -rotate-2'}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <div
+                className="bg-white shadow-md rounded-lg overflow-hidden w-100"
+                data-aos="fade-left"
+            >
+                <div className="p-4 pt-10 rotate-card">
+                    <img
+                        className={`w-48 h-48 rounded-full mx-auto ${isHovered ? '' : 'transform'
+                            }`}
+                        src={image}
+                        alt="Profile"
+                    />
+                    <h2 className="text-xl font-semibold text-center mt-4">{name}</h2>
+                    <p className="text-gray-600 text-center mt-2">{email}</p>
+                </div>
+                <div className="px-6 py-4">
+                    <div className="flex justify-evenly">
+                        <div className="text-sm text-gray-600">
+                            <span className="font-semibold">1K Posts</span>
+                        </div>
+                        <div className="text-sm text-gray-600">
+                            <span className="font-semibold">10K Likes</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="px-6 py-4">
+                    <div className="flex justify-center">
+                        <a href="#" className="text-gray-500 hover:text-red-500 mx-3">
+                            <FaFacebook />
+                        </a>
+                        <a href="#" className="text-gray-500 hover:text-red-500 mx-3">
+                            <FaTwitter />
+                        </a>
+                        <a href="#" className="text-gray-500 hover:text-red-500 mx-3">
+                            <FaInstagram />
+                        </a>
+                        <a href="#" className="text-gray-500 hover:text-red-500 mx-3">
+                            <FaLinkedin />
+                        </a>
+                        <a href="#" className="text-gray-500 hover:text-red-500 mx-3">
+                            <FaGithub />
+                        </a>
+                    </div>
+                </div>
+                <div className="px-6 py-4">
+                    <button className=" bg-red-400 text-white font-semibold py-2 px-4 rounded-full w-full hover:bg-red-500 transition-all duration-200">
+                        Message
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InstructorsDetails;
